fd637f788473187d32ce98bf3f26e1fea61046b9
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-12-02 18:03:53 +0700

    Initial commit

---
+++ lib/DataDist.pm
@@ -0,0 +1,18 @@
+package DataDist;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Convention for distribution that primarily contains/shares data
+
+=head1 DESCRIPTION
+
+B<CURRENTLY EMPTY, NAME GRAB>
+
+C<DataDist::> is a namespace meant for Perl distributions that mainly
+contain/share data instead of code. This module, C<DataDist>, provides
+convention for such distributions.
+
+
+=head1 SEE ALSO
